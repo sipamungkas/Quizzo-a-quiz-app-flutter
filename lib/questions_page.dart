66a93b7ab74598dc6eeae2e3da62bc9384ae5fcc
Ragil Pamungkas <sipamungkas.dev@gmail.com>
2023-11-24 00:16:44 +0700

    refactor(drawer)

--- lib/questions_page.dart
+++ lib/questions_page.dart
@@ -29,30 +29,52 @@ class _QuestionsPageState extends State<QuestionsPage> {
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                Text(
-                  'Daftar Soal',
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-                Consumer<DataModel>(builder: (context, data, child) {
-                  return Text('Kelas ${data.grade}');
-                }),
-                const SizedBox(
-                  height: 16,
-                ),
-                Expanded(
-                  child: Builder(builder: (context) {
-                    return GridView.count(
-                      crossAxisCount: 4,
-                      mainAxisSpacing: 16,
-                      crossAxisSpacing: 16,
-                      children: [],
-                    );
-                  }),
-                ),
-              ],
-            ),
+            child: Consumer<DataModel>(builder: (context, data, child) {
+              return Column(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Hi ${data.name}',
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          'Saat ini kamu sedang mengerjakan soal untuk kelas',
+                          style: Theme.of(context).textTheme.bodyMedium,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 16),
+                        Text('Kelas ${data.grade}'),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Builder(builder: (context) {
+                      return OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(
+                              color: Theme.of(context).colorScheme.primary),
+                        ),
+                        child: Text(
+                          'close',
+                          style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      );
+                    }),
+                  )
+                ],
+              );
+            }),
           ),
         ),
       ),
